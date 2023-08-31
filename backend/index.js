2f9a6fb7cf4312e03c35ea3c42009d1ed1b75422
Huanan <haugustocanova@gmail.com>
2023-08-31 11:24:54 -0300

    first endpoint created

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const port = process.env.PORT;
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.json({
+        messege: 'Working'
+    })
+})
+
+app.listen(port);
+
+console.log("server ok");
--- backend/router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = require('./router')
-const app = express();
-
-
-router.get('/', (req, res ) => res.status(200).send('Hello World'));
-
-module.exports = router;
